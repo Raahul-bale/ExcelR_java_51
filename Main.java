2fbdee669ce04fed3aede4a06d4f9b763af71d08
Raahul <baleraahul@gmail.com>
2025-01-04 12:36:29 +0530

    daqy1

---
+++ Main.java
@@ -0,0 +1,39 @@
+
+
+
+
+ class Main{
+    public static void main(String[] args) {
+        // /
+        // System.out.println("How's you?");
+        // // +, -, %, *, /
+        // System.out.println(2+2);
+        // System.out.println(2-2);
+        // System.out.println(2*2);
+        // System.out.println(8/3); // 2 integer -> quotient
+        // System.out.println(8%3); // remainder
+        String s = "Hello", s1 = "montain", s2 = "red";
+        int lenS=s.length();
+        int lenS1=s1.length();
+        int lenS2=s2.length();
+        System.out.println(s.charAt(lenS-1));
+        System.out.println(s1.charAt(lenS1-1));
+        System.out.println(s2.charAt(lenS2-1));
+        System.out.println(s.substring(lenS - 3,lenS - 1));
+        System.out.println(s1.substring( 4));
+        System.out.println(s2.substring( 2,2));//empty string;
+        System.out.println(s.substring(lenS - 3));
+
+        // How"s you?
+
+        // Scanner sc=new Scanner(System.in);
+        // int n=sc.nextInt();
+        // int sum = 0;
+        // int i=1;
+        // for(;i <= n; i++){
+        //     sum += i;
+        // }
+        // System.err.print("sum");
+        // System.err.print("sum");
+    }}
+ 
