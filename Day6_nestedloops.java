a4a91423cf9800e3ad01e212d642575db330a0ed
Raahul <baleraahul@gmail.com>
2025-01-10 22:06:14 +0530

    day 7

--- Day6_nestedloops.java
+++ Day6_nestedloops.java
@@ -1,26 +1,26 @@
-public class Day6_nestedloops {
-    public static int[] twoSum int arr[], int target){
-        int[] result = new int[2];
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]==target){
-                    result[0]=i;
-                    result[1]=j;
-                    return result;
-                }
-            }
-        }
-        return result;
-    }
-    public static void main(String[] args) {
-        int arr[]={2,7,11,15};
-        int target =9;
-        int[] result=twoSum(arr, target);
-        for(int i=0;i<result.length;i++){
-            System.out.println(result[i]);
-        }
-    }
-}
+// public class Day6_nestedloops {
+//     public static int[] twoSum int arr[], int target){
+//         int[] result = new int[2];
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=i+1;j<arr.length;j++){
+//                 if(arr[i]+arr[j]==target){
+//                     result[0]=i;
+//                     result[1]=j;
+//                     return result;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+//     public static void main(String[] args) {
+//         int arr[]={2,7,11,15};
+//         int target =9;
+//         int[] result=twoSum(arr, target);
+//         for(int i=0;i<result.length;i++){
+//             System.out.println(result[i]);
+//         }
+//     }
+// }
 
 
         // for(int i=1;i<=4;i++){
